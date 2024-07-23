4a543c25ea645c4b3cf5725ec1d5ec546056061a
Joakim Eineving <Joakim.eineving@gmail.com>
2024-07-23 12:58:04 +0200

    Ocelot

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseOcelot().Wait();
 
 app.Run();
